00f03a05c55f02e1957506c09db02c8287bc64fa
minsangkimme <minsang.tech@gmail.com>
2022-02-14 13:20:49 +0900

    add cardAddForm component

--- src/components/card_edit_form copy/card_edit_form.jsx
+++ src/components/card_edit_form copy/card_edit_form.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Button from '../button/button';
 import styles from './card_edit_form.module.css';
-import ImageFileInput from './../image_file_input/image_file_input';
+import ImageFileInput from '../image_file_input/image_file_input';
 
 const CardEditForm = ({ card }) => {
   const {name, company, title, email, message, theme, fileName, fileURL} = card;
@@ -13,9 +13,9 @@ const CardEditForm = ({ card }) => {
       <input className={styles.input} type="text" name="name" value={name} />
       <input className={styles.input} type="text" name="company" value={company} />
       <select className={styles.select} name="theme" value={theme}>
-        <option value="light">light</option>
-        <option value="dark">dark</option>
-        <option value="colorful">colorful</option>
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+        <option value="colorful">Colorful</option>
       </select>
       <input className={styles.input} type="text" name="title" value={title} />
       <input className={styles.input} type="text" name="email" value={email} />
